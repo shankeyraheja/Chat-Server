41f7a4530392b28e2a4da7d0134a776287f486a6
shankeyraheja <shankeyraheja@gmail.com>
2021-05-16 05:51:49 +1200

    backend code correction

--- server.js
+++ server.js
@@ -47,9 +47,9 @@ io.on("connect", (socket) =>{
   socket.on("join", ({name, room}, callback) => {
     const {error, user} = addUser({id:socket.id, name:name, room:room})
     if(error) return callback(error)
+    socket.join(user.room)
     socket.emit("message", {user:"admin", text: `Welcome to the room ${user.room}`})
     socket.broadcast.to(user.room).emit("message", {user:"admin", text: `${user.name} has joined`})
-    socket.join(user.room)
     io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
     console.log(getUsersInRoom(user.room))
     callback()
@@ -60,8 +60,11 @@ io.on("connect", (socket) =>{
     callback()
   })
   socket.on("disconnect", () => {
-    const user = getUser(socket.id)
-    socket.broadcast.to(user.room).emit("message", {user:"admin", text: `${user.name} has left`})
+    const user = removeUser(socket.id)
+    if(user){
+    io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
+    io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+  }
   })
 })
 
